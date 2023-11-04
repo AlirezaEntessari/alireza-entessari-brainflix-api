e2d5f031cef79abdd8583aeeafd3af172e4fc28a
aent <alintssr@gmail.com>
2023-11-04 12:51:51 -0400

    added static images and fixed route configuration

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static('./public'));
+
+// Pull in the dotenv package and configure it
+require("dotenv").config();
+
+// Get the PORT from the .env file, or if it wasn't provided, use 8080 as a default
+const PORT = process.env.PORT || 8080;
+
+// Allow the domain of the client to communicate with this API
+// app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 const videos = require('./routes/videos');
 
 app.use('/videos', videos);
 
-app.listen(8080, () => {
-    console.log("Listening on 8080");
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 })
--- routes/videos.js
+++ routes/videos.js
@@ -1,15 +1,80 @@
 const express = require("express");
 const fs = require("fs");
+const { parse } = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 
+const videosJson = fs.readFileSync('./data/videos.json');
+
+const parsedVideos = JSON.parse(videosJson);
+
 router.get('/', (req, res) => {
-    res.send("hello");
+    res.json(parsedVideos);
 })
 
-router.get('/:id', (req, res) => {
-    res.send("Message");
+router.get('/:videoId', (req, res) => {
+    const videoId = req.params.videoId;
+
+    const individualVideo = parsedVideos.find((video) => {
+        return video.id === videoId;
+    })
+
+    console.log(individualVideo);
+    // console.log(videoId);
+    console.log(parsedVideos);
+    // console.log(req.params);
+
+    if(!individualVideo){
+        res.status(404).send("Video not found");
+    }
+
+    res.json(individualVideo);
+})
+
+//POST an album
+//function to read albums file
+const readVideos = () => {
+    //read the JSON file containing albums data
+    const videosFile = fs.readFileSync("./data/videos.json");
+    const videos = JSON.parse(videosFile);
+    return videos;
+  };
+
+//function to write to albums file
+const writeVideos = (data) => {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFileSync("./data/videos.json", stringifiedData);
+  
+  };
+
+
+router.post('/', (req, res) => {
+
+    console.log(req.body);
+
+    const videos = readVideos();
+
+    const newVideo = {
+        id: uuidv4(),
+        title: req.body.title,
+        timestamp: new Date(),
+        description: req.body.description,
+        views: 0,
+        likes: 0,
+        comments: [],
+        channel: req.body.channel,
+        image: 'images/Upload-video-preview.jpg'
+      };
+
+    videos.push(newVideo);
+
+    writeVideos(videos);
+
+    res.json(newVideo);
+
+    res.json('success');
+    
 })
 
 module.exports = router;
